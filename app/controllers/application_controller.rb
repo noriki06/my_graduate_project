8fca53510f497023460901a8e4314586be34f822
大平尭宜 <oohiranoriki@oohiratakashiyonoMacBook-Air.local>
2026-02-08 22:13:07 +0900

    プロフィール編集画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :birthday ])
   end
 
+  def after_sign_in_path_for(_resource)
+    wants_path
+  end
+
   private
 
   def set_locale
