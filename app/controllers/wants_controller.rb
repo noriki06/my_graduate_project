a8ea6d4dd72d3650a3dbaced1b970d3d1cf6ed82
大平尭宜 <oohiranoriki@oohiratakashiyonoMacBook-Air.local>
2026-02-21 17:55:42 +0900

    fix

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -8,10 +8,16 @@ class WantsController < ApplicationController
     wants = current_user.wants.for_list
     grouped = wants.group_by(&:time_bucket)
 
-    current_age = ((Date.current - current_user.birthday.to_date).to_i / 365.25).floor
     max_age = 89
+    birthday = current_user.birthday
 
-    all_buckets = Want.time_buckets_between(from_age: current_age, to_age: max_age)
+    all_buckets =
+      if birthday.present?
+        current_age = ((Date.current - birthday.to_date).to_i / 365.25).floor
+        Want.time_buckets_between(from_age: current_age, to_age: max_age)
+      else
+        [] # もしくは Want.time_buckets_between(from_age: 20, to_age: max_age)
+      end
 
     @wants_by_bucket = all_buckets.index_with { [] }
 
@@ -66,10 +72,10 @@ class WantsController < ApplicationController
     already_achieved = @want.achieved?
 
     if @want.achieve(
-        achievement_note: params.dig(:want, :achievement_note),
-        achieved_at_input: params.dig(:want, :achieved_at),
-        now: Time.current
-      )
+      achievement_note: params.dig(:want, :achievement_note),
+      achieved_at_input: params.dig(:want, :achieved_at),
+      now: Time.current
+    )
       notice = already_achieved ? "思い出メモを更新しました！" : "達成を記録しました！"
       redirect_to wants_path, notice: notice
     else
@@ -87,16 +93,17 @@ class WantsController < ApplicationController
     params.require(:want).permit(:title, :memo, :target_date)
   end
 
-  # 🔥 ここが今回の追加部分
+  # 年齢が入力されたときだけ target_date を計算してセットする
+  # （日付入力がある場合はそれを尊重する）
   def apply_target_age!(want)
     age_str = params[:target_age].presence
     return if age_str.blank?
+    return if want.target_date.present? # ← 日付入力が優先
     return if current_user.birthday.blank?
 
     age = age_str.to_i
     return if age <= 0
 
-    # 誕生日 + age年
     want.target_date = current_user.birthday.to_date + age.years
   end
 end
