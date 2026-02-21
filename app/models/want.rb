f9a4168aff929282e41de733ec8da3fe8074d475
大平尭宜 <oohiranoriki@oohiratakashiyonoMacBook-Air.local>
2026-02-21 11:01:55 +0900

    Add time_bucket to wants and auto-generate by target_date

--- app/models/want.rb
+++ app/models/want.rb
@@ -3,6 +3,9 @@ class Want < ApplicationRecord
 
   validates :title, presence: true
 
+  TIME_BUCKET_SPAN = 5
+  before_validation :set_time_bucket, if: :should_set_time_bucket?
+
   scope :for_list, -> {
     order(Arel.sql("achieved_at IS NOT NULL ASC"))
       .order(achieved_at: :desc, created_at: :desc)
@@ -16,9 +19,7 @@ class Want < ApplicationRecord
     attrs = { achievement_note: achievement_note }
 
     if achieved?
-      if achieved_at_input.present?
-        attrs[:achieved_at] = parse_achieved_at(achieved_at_input, now)
-      end
+      attrs[:achieved_at] = parse_achieved_at(achieved_at_input, now) if achieved_at_input.present?
       update(attrs.compact_blank)
     else
       attrs[:achieved_at] = parse_achieved_at(achieved_at_input, now)
@@ -26,17 +27,36 @@ class Want < ApplicationRecord
     end
   end
 
+  # 例）27 -> "25-29"
+  def self.time_bucket_for_age(age, span: TIME_BUCKET_SPAN)
+    return nil if age.nil?
+
+    start_age = (age / span) * span
+    end_age = start_age + (span - 1)
+    "#{start_age}-#{end_age}"
+  end
+
   private
+
+  def set_time_bucket
+    return if target_date.blank?
+    return if user.blank? || user.birthday.blank?
+
+    age = ((target_date.to_date - user.birthday.to_date).to_i / 365.25).floor
+    self.time_bucket = self.class.time_bucket_for_age(age)
+  end
+
+  def should_set_time_bucket?
+    time_bucket.blank? || will_save_change_to_target_date? || will_save_change_to_user_id?
+  end
 
   def parse_achieved_at(input, now)
     return now.end_of_day if input.blank?
 
     time =
       case input
-      when Time
-        input
-      when Date
-        input.in_time_zone
+      when Time then input
+      when Date then input.in_time_zone
       else
         begin
           Time.zone.parse(input.to_s)
